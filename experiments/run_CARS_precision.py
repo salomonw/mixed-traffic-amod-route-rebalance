add test and exp, fixed some issues on CARS precision

experiments/run_CARS_precision.py
@@ -8,26 +8,32 @@ netFile = "data/net/EMA_net.txt"
 gFile = "data/trips/EMA_trips.txt"
 fcoeffs = [1,0,0,0,0.15,0]
 demand_multiplier = list(np.linspace(0.5,3,25))
+xa = 1
 
 
 cars_obj = []
 real_obj = []
 diff = []
+flow_diff = []
 for g_multi in demand_multiplier:
     tNet = tnet.tNet(netFile=netFile, gFile=gFile, fcoeffs=fcoeffs)
+    g_per = tnet.perturbDemandConstant(tNet.g, g_multi)
+    tNet.set_g(g_per)
+
     tNet.build_supergraph()
     pedestrian = [(u, v) for (u, v, d) in tNet.G_supergraph.edges(data=True) if d['type'] == 'p']
     connector = [(u, v) for (u, v, d) in tNet.G_supergraph.edges(data=True) if d['type'] == 'f']
 
-    g_per = tnet.perturbDemandConstant(tNet.g, g_multi)
-    tNet.set_g(g_per)
-
-    cars.solve_CARS2(tNet, fcoeffs=fcoeffs, exogenous_G=False, xa=1.2)
+    cars.solve_CARS2_noRebalancing(tNet, fcoeffs=fcoeffs, exogenous_G=False, xa=xa)
+    tNet.solveMSAsocial()
 
     print(g_multi)
-    cars_obj.append(cars.get_totalTravelTime_approx(tNet, fcoeffs=fcoeffs, xa=1.2))
-    real_obj.append(tnet.get_totalTravelTime(tNet.G_supergraph, fcoeffs))
-    diff.append(abs(tnet.get_totalTravelTime(tNet.G_supergraph, fcoeffs)-cars.get_totalTravelTime_approx(tNet, fcoeffs=fcoeffs, xa=1.2)))
+    carsObj = tnet.get_totalTravelTime(tNet.G_supergraph, fcoeffs)
+    socialObj = tnet.get_totalTravelTime(tNet.G, fcoeffs)
+    cars_obj.append(carsObj)
+    real_obj.append(socialObj)
+    diff.append(abs(carsObj-socialObj))
+    flow_diff.append(tnet.normFlowDiff(tNet.G, tNet.G_supergraph))
 
 plt.figure()
 plt.plot(demand_multiplier, real_obj, label="BPR")
@@ -39,5 +45,11 @@ plt.figure()
 plt.plot(demand_multiplier[0:15], real_obj[0:15], label="BPR")
 plt.plot(demand_multiplier[0:15], cars_obj[0:15], label="Approx")
 plt.plot(demand_multiplier[0:15], diff[0:15], label='Approximation error')
+plt.legend()
+plt.show()
+
+
+plt.figure()
+plt.plot(demand_multiplier, flow_diff, label='Flow norm')
 plt.legend()
 plt.show()

experiments/test_CARS.py
@@ -28,12 +28,14 @@ for g_multiplier in np.linspace(0.025, 3, 20):
     pedestrian = [(u, v) for (u, v, d) in tNet.G_supergraph.edges(data=True) if d['type'] == 'p']
     connector = [(u, v) for (u, v, d) in tNet.G_supergraph.edges(data=True) if d['type'] == 'f']
     g_k = tnet.perturbDemandConstant(tNet.g, constant=g_multiplier)
-    tNet.solveMSA()
     totalObj = []
     for j in range(10):
         tNet.set_g(g_k)
-        tNet = cars.solve_CARS2(tNet, tNet.G, fcoeffs=fcoeffs, xa=0.8)
-        tNet.solveMSA(exogenous_G=tNet.G_supergraph)
+        if j==0:
+            tNet.solveMSA()
+        else:
+            tNet.solveMSA(exogenous_G=tNet.G_supergraph)
+        tNet = cars.solve_CARS2(tNet, exogenous_G=tNet.G, fcoeffs=fcoeffs, xa=0.8)
         exogObj = tnet.get_totalTravelTime(tNet.G, fcoeffs)
         amodObj = cars.get_totalTravelTime(tNet)
         amodObjNoRebalancing = cars.get_totalTravelTime_without_Rebalancing(tNet)