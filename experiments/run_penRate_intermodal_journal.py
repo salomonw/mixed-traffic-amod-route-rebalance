a390057e904af8f610e1cb5482fbea9b7f857615
Salomon Wollenstein <salomonw@scc1.bu.edu>
2020-12-10 17:47:15 -0500

    to git

--- experiments/run_penRate_intermodal_journal.py
+++ experiments/run_penRate_intermodal_journal.py
@@ -70,6 +70,7 @@ def solve_stackelberg_game(par):
   
     it = []
     for i in range(iterations):
+        '''
         if i <= 0:
             cars.solve_bush_CARSn(tnet=tNet_non_cavs, fcoeffs=fcoeffs, n=n, exogenous_G=False,
 	                          rebalancing=rebalancing, bush=True, linear=linear,
@@ -80,7 +81,8 @@ def solve_stackelberg_game(par):
 				  rebalancing=rebalancing, bush=True, linear=linear, 
 				  theta_n=theta_n, theta=theta_non_cavs, a=a_non_cavs, od_flows_flag=False, 
 				  userCentric=True)
-        if i <=-10:
+        '''
+        if i <=0:
             cars.solve_bush_CARSn(tnet=tNet_cavs, fcoeffs=fcoeffs, n=n, exogenous_G=False,
                                   rebalancing=rebalancing, bush=True, linear=linear,
                                   theta_n=theta_n, theta=theta_cavs, a=a_cavs, od_flows_flag=False)
@@ -88,18 +90,18 @@ def solve_stackelberg_game(par):
             cars.solve_bush_CARSn(tnet=tNet_cavs, fcoeffs=fcoeffs, n=n, exogenous_G=tNet_non_cavs.G_supergraph,
                                   rebalancing=rebalancing, bush=True, linear=linear,
                                   theta_n=theta_n, theta=theta_cavs, a=a_cavs, od_flows_flag=False)
-        #if i <=0:
+        if i <=-10:
             #tNet_non_cavs.solveMSA(exogenous_G=False)
-        #    cars.solve_bush_CARSn(tnet=tNet_non_cavs, fcoeffs=fcoeffs, n=n,
-        #                          rebalancing=False, bush=True, userCentric=True,
-        #                          exogenous_G=False, linear=linear,
-        #                          theta_n=theta_n, theta=theta_non_cavs, a=a_non_cavs, od_flows_flag=False)
-        #else:
+            cars.solve_bush_CARSn(tnet=tNet_non_cavs, fcoeffs=fcoeffs, n=n,
+                                  rebalancing=False, bush=True, userCentric=True,
+                                  exogenous_G=False, linear=linear,
+                                  theta_n=theta_n, theta=theta_non_cavs, a=a_non_cavs, od_flows_flag=False)
+        else:
             #tNet_non_cavs.solveMSA(exogenous_G=tNet_cavs.G_supergraph)
-        #    cars.solve_bush_CARSn(tnet=tNet_non_cavs, fcoeffs=fcoeffs, n=n,
-        #                          rebalancing=False, bush=True, userCentric=True,
-        #                          exogenous_G=tNet_cavs.G_supergraph, linear=linear,
-        #                          theta_n=theta_n, theta=theta_non_cavs, a=a_non_cavs, od_flows_flag=False)
+            cars.solve_bush_CARSn(tnet=tNet_non_cavs, fcoeffs=fcoeffs, n=n,
+                                  rebalancing=False, bush=True, userCentric=True,
+                                  exogenous_G=tNet_cavs.G_supergraph, linear=linear,
+                                  theta_n=theta_n, theta=theta_non_cavs, a=a_non_cavs, od_flows_flag=False)
         #cars.G2supergraph(tNet_non_cavs)
         #cars.supergraph2G(tNet_non_cavs)
 
@@ -263,7 +265,7 @@ modes = str(sys.argv[3])
 rebalancing = True
 n = 5
 n_iter = 5
-theta_n = 2.5
+theta_n = 3
 linear = False
 parallel = True
 
